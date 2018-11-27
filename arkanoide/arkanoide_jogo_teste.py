27aee012f5bf8b67810014156fe7e7e43e2677cb
professor <clayton-cerci@hotmail.com>
2018-11-27 21:35:53 -0200

    arrumado a colisao com o bloco, a bola nao inverte de direção

--- arkanoide/arkanoide_jogo_teste.py
+++ arkanoide/arkanoide_jogo_teste.py
@@ -13,3 +13,8 @@ class Test(unittest.TestCase):
     def testcolide_bola(self):
         self.assertEqual(colide_bola(Bola(400, 0, 150, 6), BARRA_INICIAL), 0)
         self.assertEqual(colide_bola(Bola(BARRA_INICIAL.x - BARRA_INICIAL.tam / 2 - 1, 0, Y - altura_imagem(IMG_BARRA)//2, 6), BARRA_INICIAL),1)
+
+
+    def testcolide_bloco(self):
+        self.assertEqual(colide_bloco(Bola(400, 0, 150, 6), BLOCO1), False)
+        self.assertEqual(colide_bloco(Bola(400, 0, 300, 6), BLOCO1), True)
